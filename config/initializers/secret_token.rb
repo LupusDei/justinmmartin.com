8abcb63ce61f2d8366fb4db63bf0d420f523fb61
Justin Martin <lupusdei108@gmail.com>
2013-01-05 17:53:40 -0500

    Restarting the website to improve it and get some practice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustinmmartinCom::Application.config.secret_token = '72cbad9c1ec5b449dc88adc651416c31d244743e9a4268046e176ba2a21fdb14aa5e6e7fed52694346ef614b1e475b51052919ae63b465e826ccd8d031eb567b'
